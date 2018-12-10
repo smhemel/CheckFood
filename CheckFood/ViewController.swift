e63fce3996e66629b3267b311f2638ebdb2bfbd4
smhemel <sm_hemel@yahoo.com>
2018-12-10 16:38:08 +0600

    Initial Commit

---
+++ CheckFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckFood
+//
+//  Created by S M HEMEL on 10/12/18.
+//  Copyright © 2018 S M HEMEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
